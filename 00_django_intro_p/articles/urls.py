c15f8e12292a69241ebfe4fdd549136ef190508c
gyeol <gksruf1128@naver.com>
2021-03-09 10:10:54 +0900

    Intro Django practice

---
+++ 00_django_intro_p/articles/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index/', views.index , name='index'),
+    path('ping/', views.ping, name='ping'),
+    path('pong/', views.pong, name='pong'),
+    path('<int:num>/', views.detail, name='detail'),
+]
